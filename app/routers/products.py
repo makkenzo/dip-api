7251a43b4e742e4a5af27adbd19e97412061ac05
makkenzo <nekgo2009@gmail.com>
2024-02-16 19:53:39 +0600

    Add pagination and filtering to get_all_products endpoint

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,18 +1,27 @@
 from fastapi import APIRouter
 from ..utils.db import get_products
+from fastapi import APIRouter, Query
+from typing import List
 
-router = APIRouter(prefix='/products', tags=['Products'])
+
+router = APIRouter(prefix="/products", tags=["Products"])
 
 
-@router.get('/get-products')
-async def get_all_products():
+@router.get("/get-products")
+async def get_all_products(page: int = Query(1, ge=1), limit: int = Query(4, ge=1, le=100), filter: str = Query(None)):
     try:
         db_products = get_products()
 
-        products_cursor = await db_products.find({}).to_list(length=None)
+        if filter:
+            products_cursor = await db_products.find({"name": {"$regex": filter, "$options": "i"}}).to_list(length=None)
+        else:
+            products_cursor = await db_products.find({}).to_list(length=None)
 
-        products = [{**product, "_id": str(product["_id"])}
-                    for product in products_cursor]
+        start_index = (page - 1) * limit
+        end_index = start_index + limit
+        products_cursor = products_cursor[start_index:end_index]
+
+        products = [{**product, "_id": str(product["_id"])} for product in products_cursor]
 
         return {"data": products, "count": len(products)}
     except Exception as e:
