52430781bcdccb8d91058bd55923bbfe5e353f9e
makkenzo <nekgo2009@gmail.com>
2024-03-27 18:31:07 +0500

    test

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,6 +1,11 @@
+from typing import Any
 from pydantic import BaseModel
 
 
 class RegistrationModel(BaseModel):
-    username: str
-    password: str
+    data: Any
+    object: str
+    type: str
+
+    class Config:
+        allow_population_by_field_name = True
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,6 +5,8 @@ from ..models.auth import RegistrationModel
 
 router = APIRouter(prefix="/auth", tags=["Authorization"])
 
+wh_secret = "whsec_Hxa5+Yn0xsu6jmM50VbJPygrSsijwWNK"
+
 
 @router.post("/sign-up")
 async def sign_up(req: Request, req_body: RegistrationModel):
